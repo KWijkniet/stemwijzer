bec292e10819b2e23c537a79157cf7583f1fe2de
Kelvin Wijkniet <kwijkniet@outlook.com>
2019-02-06 18:28:59 +0100

    start question priority

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,31 @@
-//Get elements (requires to have "display:none;" as default)
+//show subjects in console (just a test)
+console.log(subjects);
+
+//Get pages (requires to have "display:none;" as default)
 const introElem = document.getElementById('intro');
 const questionElem = document.getElementById('question');
 const resultElem = document.getElementById('result');
 const partijenElem = document.getElementById('partijen');
+const priority = document.getElementById('priority');
+const myProgress = document.getElementById('myProgress');
 
-//question parts
-const questionTitle = document.getElementById('questionTitle');
-const questionInfo = document.getElementById('questionInfo');
-
+//party rows in dropdown
 const eensRow = document.getElementById('eensRow');
 const noneRow = document.getElementById('noneRow');
 const oneensRow = document.getElementById('oneensRow');
 
-//show subjects in console (just a test)
-console.log(subjects);
+//question parts
+var questionTitle = document.getElementById('questionTitle');
+var questionInfo = document.getElementById('questionInfo');
 
-//index keep track of subject index (starts at 0)
+//priority elements
+var priorityList = document.getElementById('priority-container');
+
+//progress bar
+var progressBar = document.getElementById('myBar');
+progressBar.style.width = (100 / (subjects.length + 2) * 1) + "%";
+
+//index keep track of subjects index (starts at 0)
 var index = 0;
 var isOpen = false;
 var answers = [];
@@ -24,13 +34,13 @@ var answers = [];
 ShowIntro();
 function ShowIntro(){
     //hide questions page and show intro page
+    HidePages();
     introElem.style.display = 'block';
-    questionElem.style.display = 'none';
-    resultElem.style.display = 'none';
 }
 
 //start questions
 function StartQuiz(){
+    progressBar.style.width = (100 / (subjects.length + 2) * 1) + "%";
     index = 0;
     ShowQuestion();
 }
@@ -39,9 +49,9 @@ function StartQuiz(){
 function ShowQuestion(){
     //fix displays and heights
     partijenElem.style.height = "0px";
-    introElem.style.display = 'none';
+    HidePages();
     questionElem.style.display = 'block';
-    resultElem.style.display = 'none';
+    myProgress.style.display = 'block';
 
     //show question data on the page
     questionTitle.innerHTML = (index + 1) + ". " + subjects[index].title;
@@ -53,9 +63,9 @@ function ShowQuestion(){
 //shows end page with the result
 function ShowEnd(){
     //hide questions page and show intro page
-    introElem.style.display = 'none';
-    questionElem.style.display = 'none';
+    HidePages();
     resultElem.style.display = 'block';
+    myProgress.style.display = 'block';
 }
 
 //public function for the next button
@@ -68,14 +78,14 @@ function NextQuestion(value){
     };
     answers[index] = row;
 
+    progressBar.style.width = (100 / (subjects.length + 2) * (index + 2)) + "%";
 
     //increase index
     index++;
 
     //if index is larger then subjects length. then show the result page. else show the next question
     if(index >= subjects.length){
-        ShowEnd();
-        console.log(answers);
+        GeneratePriorityList();
     }else{
         ShowQuestion();
     }
@@ -86,6 +96,7 @@ function PrevQuestion(){
     //decrease index
     index--;
 
+    progressBar.style.width = (100 / (subjects.length + 2) * (index + 1)) + "%";
     //if index is smaller then 0. then show the start page. else show the previous question
     if(index < 0){
         ShowIntro();
@@ -116,12 +127,35 @@ function GeneratePartyVotes(){
     for(var i = 0; i < subjects[index].parties.length; i++){
         var party = subjects[index].parties[i];
         //place party in correct place (eens, geen van beide, oneens);
+        var htmlLeft = "<li class='party-opinion'>" + party.name + "<span class='tooltiptext tooltiptext-left'>" + party.explanation + "</span</li>";
+        var htmlRight = "<li class='party-opinion'>" + party.name + "<span class='tooltiptext tooltiptext-right'>" + party.explanation + "</span</li>";
+
         if(party.position == 'pro'){
-            eensRow.innerHTML += "<li>" + party.name + "</li>";
+            eensRow.innerHTML += htmlRight;
         } else if(party.position == 'ambivalent'){
-            noneRow.innerHTML += "<li>" + party.name + "</li>";
+            noneRow.innerHTML += htmlRight;
         } else if(party.position == 'contra'){
-            oneensRow.innerHTML += "<li>" + party.name + "</li>";
+            oneensRow.innerHTML += htmlLeft;
         }
     }
+}
+
+function GeneratePriorityList(){
+    HidePages();
+    priority.style.display = 'block';
+    myProgress.style.display = 'block';
+
+    priorityList.innerHTML = '';
+    for(var i = 0; i < subjects.length; i++){
+        var html = "<label class='checkbox-container'>" + subjects[i].title + "<input type='checkbox' value='" + i + "'><span class='checkmark'></span></label>";
+        priorityList.innerHTML += html;
+    }
+}
+
+function HidePages(){
+    introElem.style.display = 'none';
+    questionElem.style.display = 'none';
+    resultElem.style.display = 'none';
+    priority.style.display = 'none';
+    myProgress.style.display = 'none';
 }
